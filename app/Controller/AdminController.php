17dc7f89023bfd7e246ffcaad46488810a3bfa98
Abhishek Shroff <me@shroffabhishek.net>
2012-04-03 12:31:41 -0400

    Removed unnecessary files from admin

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -7,32 +7,15 @@ class AdminController extends AppController {
   public function index() {
     foreach(array('ReportedAnswer', 'ReportedQuestion') as $reportType){
       $this->set($reportType . 's', $this->$reportType->find('all'));
-      //debug($this->viewVars, true);
     }    
+    $this->set('answers', $this->ReportedAnswer->find('all'));
+    $this->set('questions', $this->ReportedQuestion->find('all'));
   }
 
   public function users() {
     $this->set('users', $this->User->find('all'));
     //$cond = array('answer_count' => 0);
     //$this->set('questions', $this->Question->find('all', array('conditions' => $cond)));
-  }
-
-  public function view($id = null) {
-    $this->Question->id = $id;
-    $q = $this->Question->read();
-    $this->request->data['Answer']['question_id'] = $id;
-    if($q == null) {
-      $this->Session->setFlash('This question does not exist or has been deleted');
-      $this->redirect('/questions');
-    }
-
-    for ($i=count($q['QuestionAnswer'])-1; $i >= 0; $i--) {
-      $this->User->id = $q['QuestionAnswer'][$i]['user_id'];
-      $user = $this->User->read();
-      $q['QuestionAnswer'][$i]['username'] = $user['User']['username'];
-    }
-
-    $this->set('question', $q);
   }
 
 }
