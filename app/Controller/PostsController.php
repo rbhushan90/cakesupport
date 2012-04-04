94da10d6ad161ed3ac663cdc66d15920139719a4
Brian Gardner <brngardner@gmail.com>
2012-04-04 18:05:00 -0400

    Insert data into post tag table when a post is created, and remove when post is deleted

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -3,30 +3,55 @@
 class PostsController extends AppController {
   public $name = 'Post';
   public $helpers = array('Html', 'Form');
-  public $uses = array('Post', 'User');
+  public $uses = array('Post', 'User', 'Tag', 'PostTag');
 
   public function add() {
     $this->User;
-    if(!($this->Session->read('User.permissions') & Configure::read('permissions.postBlog'))) {
+    if(($this->Session->read('User.permissions') & Configure::read('permissions.postBlog'))) {
       $this->Session->setFlash('You do not have the permissions to post to the blog');
       $this->redirect('/blog');
     }
-    if($this->request->data) {
+    $tags = $this->Tag->find('all');
+    if($this->request->is('get')) {
+      $t = array();
+      foreach($tags as $tag){
+        $t[$tag['Tag']['id']] = $tag['Tag']['name'];
+      }
+      $this->set('tags', $t);
+    }
+    if($this->request->is('post') && $this->request->data) {
       $this->Session->setFlash('The post has been added.');
       include '../webroot/markitup/markdown.php';
       $this->request->data['Post']['output'] = Markdown($this->request->data['Post']['body']);
       $this->request->data['Post']['user_id'] = $this->Session->read('User.id');
       $this->Post->save($this->request->data);
+      $pid = $this->Post->id;
+      foreach($tags as $tag){
+        $name = $tag['Tag']['name'];
+        if($this->request->data['Post'][$name]==1){
+          // This tag was selected, add record to PostTag db table
+          $this->PostTag->create();
+          $this->PostTag->set(array(
+            'post_id' => $pid,
+            'tag_id' => $tag['Tag']['id']
+          ));
+          $this->PostTag->save();
+        }
+      }
       $this->redirect('/blog');
     }
   }
 
   public function delete($id = null) {
-    if(!($this->Session->read('User.permissions') & Configure::read('permissions.postBlog'))) {
+    if(($this->Session->read('User.permissions') & Configure::read('permissions.postBlog'))) {
       $this->Session->setFlash('You do not have permission to delete blog posts');
       $this->redirect('/blog');
     } else {
       $this->Post->delete($id);
+      $post_tags = $this->PostTag->find('all', array('conditions'=>array('post_id'=>$id)));
+      foreach($post_tags as $pt){
+        $this->PostTag->delete($pt['PostTag']['id']);
+      }
       $this->redirect('/blog');
     }
   }
